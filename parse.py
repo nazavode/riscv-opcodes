7f355578dd3fbd5b67c22ddecb2006f479c93e54
Federico Ficarelli <1379149+nazavode@users.noreply.github.com>
2024-05-14 18:07:04 +0200

    Add pseudo ref to parser output

--- parse.py
+++ parse.py
@@ -326,6 +326,8 @@ def create_inst_dict(file_filter, include_pseudo=False, include_pseudo_ops=[]):
 
 
             (name, single_dict) = process_enc_line(pseudo_inst + ' ' + line, f)
+            # add reference to original instruction
+            single_dict['is_pseudo_of'] = {"extension": ext, "instruction": orig_inst}
             # add the pseudo_op to the dictionary only if the original
             # instruction is not already in the dictionary.
             if orig_inst.replace('.','_') not in instr_dict \
